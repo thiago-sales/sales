f1c5e121d00bb8366825960b1d410ca4a4298e37
Thiago Sales <thiagosales2026@gmail.com>
2022-11-24 21:55:27 -0300

    box2

--- src/components/Ingles.js
+++ src/components/Ingles.js
@@ -3,20 +3,63 @@ const Ingles = () => {
             <h1 className="all">Inglês</h1>
                 <h3 className="subtitle">Bem-vindos!</h3>
 
+<div class="row row-cols-1 row-cols-md-3 g-4" id="espaco-card">
+  <div class="col">
+    <div class="card">
+      <img src="https://media.istockphoto.com/id/1370810363/photo/close-up-of-the-us-flag-waving-in-the-wind.jpg?s=612x612&w=0&k=20&c=X71eV4VGpZhO8lVpIsg7lMv4gJgGRvmz2szN8BmgsPs=" 
+        class="card-img-top" alt="..."/>
+        <div class="card-body">
+            <h5 class="card-title">6ºA - Atividade 1</h5>
+                <p class="card-text">Esta atividade deve ser entregue até segunda feira 30/11/2022.</p>
+                    <a class="btn btn-dark" target='_blank' href='https://drive.google.com/file/d/1R4GScyufYgIJ-zz5tPGVVS7bUVMrAa1g/view?usp=share_link'>
+                        Atividade 1
+                    </a>
+        </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="https://media.istockphoto.com/id/1370810363/photo/close-up-of-the-us-flag-waving-in-the-wind.jpg?s=612x612&w=0&k=20&c=X71eV4VGpZhO8lVpIsg7lMv4gJgGRvmz2szN8BmgsPs=" 
+        class="card-img-top" alt="..."/>
+        <div class="card-body">
+            <h5 class="card-title">6ºA - Atividade 1</h5>
+                <p class="card-text">Esta atividade deve ser entregue até segunda feira 30/11/2022.</p>
+                    <a class="btn btn-dark" target='_blank' href='https://drive.google.com/file/d/1R4GScyufYgIJ-zz5tPGVVS7bUVMrAa1g/view?usp=share_link'>
+                        Atividade 2
+                    </a>
+        </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="https://media.istockphoto.com/id/1370810363/photo/close-up-of-the-us-flag-waving-in-the-wind.jpg?s=612x612&w=0&k=20&c=X71eV4VGpZhO8lVpIsg7lMv4gJgGRvmz2szN8BmgsPs=" 
+        class="card-img-top" alt="..."/>
+        <div class="card-body">
+            <h5 class="card-title">6ºA - Atividade 1</h5>
+                <p class="card-text">Esta atividade deve ser entregue até segunda feira 30/11/2022.</p>
+                    <a class="btn btn-dark" target='_blank' href='https://drive.google.com/file/d/1R4GScyufYgIJ-zz5tPGVVS7bUVMrAa1g/view?usp=share_link'>
+                        Atividade 3
+                    </a>
+        </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="https://media.istockphoto.com/id/1370810363/photo/close-up-of-the-us-flag-waving-in-the-wind.jpg?s=612x612&w=0&k=20&c=X71eV4VGpZhO8lVpIsg7lMv4gJgGRvmz2szN8BmgsPs=" 
+        class="card-img-top" alt="..."/>
+        <div class="card-body">
+            <h5 class="card-title">6ºA - Atividade 1</h5>
+                <p class="card-text">Esta atividade deve ser entregue até segunda feira 30/11/2022.</p>
+                    <a class="btn btn-dark" target='_blank' href='https://drive.google.com/file/d/1R4GScyufYgIJ-zz5tPGVVS7bUVMrAa1g/view?usp=share_link'>
+                        Atividade 4
+                    </a>
+        </div>
+    </div>
+  </div>
 
-                
-                <p className="texto">
-                    Boa noite, este é um texto de teste. Boa noite, este é um texto de teste.
-                    Boa noite, este é um texto de teste. Boa noite, este é um texto de teste.
-                    Boa noite, este é um texto de teste. Boa noite, este é um texto de teste.
-                    Boa noite, este é um texto de teste. Boa noite, este é um texto de teste.
-                    Boa noite, este é um texto de teste. Boa noite, este é um texto de teste.
-                    Boa noite, este é um texto de teste. Boa noite, este é um texto de teste.
-                    Boa noite, este é um texto de teste. Boa noite, este é um texto de teste.
-                    Boa noite, este é um texto de teste. Boa noite, este é um texto de teste.
-                    Boa noite, este é um texto de teste. Boa noite, este é um texto de teste.
-                    Boa noite, este é um texto de teste. Boa noite, este é um texto de teste.
-                </p>
+
+
+</div>
     
     </>
 };

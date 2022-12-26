15e39d852c4927ecc23503c36f99ba5aa6ddecd8
Thiago Sales <thiagosales2026@gmail.com>
2022-12-26 10:02:19 -0300

    fix

--- src/components/Sobre.js
+++ src/components/Sobre.js
@@ -1,12 +1,11 @@
 const Sobre = () => {
     return <>
             <h1 className="all">Sobre</h1>
-                <h3 className="subtitle">Bem-vindos!</h3>
+                <h3 className="subtitle">Você está na página Sobre!</h3>
 
 
                 
                 <p className="texto">
-                    Boa noite, este é um texto de teste. Boa noite, este é um texto de teste.
                     Boa noite, este é um texto de teste. Boa noite, este é um texto de teste.
                     Boa noite, este é um texto de teste. Boa noite, este é um texto de teste.
                     Boa noite, este é um texto de teste. Boa noite, este é um texto de teste.

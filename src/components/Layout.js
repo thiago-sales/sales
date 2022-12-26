a50de16195076a9e1cd06ef87c65de2ade96bd08
Thiago Sales <thiagosales2026@gmail.com>
2022-12-26 09:54:29 -0300

    nav

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,20 +6,22 @@ import './Layout.css';
 const Layout = () =>{
     return(
         <>
-          <nav class="navbar navbar-expand-lg bg-dark">
+          <nav class="navbar navbar-dark bg-dark fixed-top">
   <div class="container-fluid">
   <Link to='/' className='link-nav'>
     <a class="navbar-brand" id='link-nav'>Prof. Thiago</a>
     </Link>
-
-    
-    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
       <span class="navbar-toggler-icon"></span>
     </button>
 
-
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
+    <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-lablledly="offcanvasDarkNavbarLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Menu</h5>
+        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+      <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
         <li class="nav-item">
         <Link to='/' className='link-nav'>
           <a class="nav-link active" aria-current="page" id='link-nav'>Home</a>
@@ -52,6 +54,7 @@ const Layout = () =>{
         </li>
       </ul>
     </div>
+  </div>
   </div>
 </nav>  
 

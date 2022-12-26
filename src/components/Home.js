721e84a7930a893d094562e16c5807f6a56100be
Thiago Sales <thiagosales2026@gmail.com>
2022-12-26 15:35:27 -0300

    dog2

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 const Home = () => {
     return <>
             <h1 className="all">Home</h1>
-                <h3 className="subtitle">Bem-vindos!</h3>
+                <h3 className="subtitle">Bem-vindos! Por favor escolha a sua matéria.</h3>
 
 
 <div class="row row-cols-1 row-cols-md-3 g-4" id="espaco-card">
@@ -10,7 +10,7 @@ const Home = () => {
       <img src="https://media.istockphoto.com/id/1370810363/photo/close-up-of-the-us-flag-waving-in-the-wind.jpg?s=612x612&w=0&k=20&c=X71eV4VGpZhO8lVpIsg7lMv4gJgGRvmz2szN8BmgsPs=" 
         class="card-img-top" alt="..."/>
         <div class="card-body">
-            <h5 class="card-title">6ºA - Atividade 1</h5>
+            <h5 class="card-title">Inglês</h5>
                 <p class="card-text">Esta atividade deve ser entregue até segunda feira 30/11/2022.</p>
                     <a class="btn btn-dark" target='_blank' href='https://drive.google.com/file/d/1R4GScyufYgIJ-zz5tPGVVS7bUVMrAa1g/view?usp=share_link'>
                         Atividade 1
@@ -20,10 +20,10 @@ const Home = () => {
   </div>
   <div class="col">
     <div class="card">
-      <img src="https://media.istockphoto.com/id/1370810363/photo/close-up-of-the-us-flag-waving-in-the-wind.jpg?s=612x612&w=0&k=20&c=X71eV4VGpZhO8lVpIsg7lMv4gJgGRvmz2szN8BmgsPs=" 
+      <img src="https://img.freepik.com/premium-vector/spain-flag-design-waving-spanish-flag-made-satin-silk-fabric-vector-illustration_191567-876.jpg" 
         class="card-img-top" alt="..."/>
         <div class="card-body">
-            <h5 class="card-title">6ºA - Atividade 1</h5>
+            <h5 class="card-title">Espanhol</h5>
                 <p class="card-text">Esta atividade deve ser entregue até segunda feira 30/11/2022.</p>
                     <a class="btn btn-dark" target='_blank' href='https://drive.google.com/file/d/1R4GScyufYgIJ-zz5tPGVVS7bUVMrAa1g/view?usp=share_link'>
                         Atividade 2
@@ -33,10 +33,10 @@ const Home = () => {
   </div>
   <div class="col">
     <div class="card">
-      <img src="https://media.istockphoto.com/id/1370810363/photo/close-up-of-the-us-flag-waving-in-the-wind.jpg?s=612x612&w=0&k=20&c=X71eV4VGpZhO8lVpIsg7lMv4gJgGRvmz2szN8BmgsPs=" 
+      <img src="http://pensaraeducacao.com.br/rbeducacaobasica/wp-content/uploads/sites/5/2017/10/M%C3%A3os-de-uma-crian%C3%A7a-tocando-flauta_destaque_texto01_n%C3%BAmero6_RBEB-redimensionada.jpg" 
         class="card-img-top" alt="..."/>
         <div class="card-body">
-            <h5 class="card-title">6ºA - Atividade 1</h5>
+            <h5 class="card-title">Flauta Doce</h5>
                 <p class="card-text">Esta atividade deve ser entregue até segunda feira 30/11/2022.</p>
                     <a class="btn btn-dark" target='_blank' href='https://drive.google.com/file/d/1R4GScyufYgIJ-zz5tPGVVS7bUVMrAa1g/view?usp=share_link'>
                         Atividade 3
@@ -46,10 +46,10 @@ const Home = () => {
   </div>
   <div class="col">
     <div class="card">
-      <img src="https://media.istockphoto.com/id/1370810363/photo/close-up-of-the-us-flag-waving-in-the-wind.jpg?s=612x612&w=0&k=20&c=X71eV4VGpZhO8lVpIsg7lMv4gJgGRvmz2szN8BmgsPs=" 
+      <img src="https://img.freepik.com/fotos-gratis/cantora-com-microfone-cantando-em-estudio_1303-26961.jpg?size=626&ext=jpg" 
         class="card-img-top" alt="..."/>
         <div class="card-body">
-            <h5 class="card-title">6ºA - Atividade 1</h5>
+            <h5 class="card-title">Canto</h5>
                 <p class="card-text">Esta atividade deve ser entregue até segunda feira 30/11/2022.</p>
                     <a class="btn btn-dark" target='_blank' href='https://drive.google.com/file/d/1R4GScyufYgIJ-zz5tPGVVS7bUVMrAa1g/view?usp=share_link'>
                         Atividade 4

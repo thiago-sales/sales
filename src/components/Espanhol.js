13865d723072e94cb4b9018dcc09028ce4bbc4cb
Thiago Sales <thiagosales2026@gmail.com>
2022-12-27 14:58:23 -0300

    DOG10

--- src/components/Espanhol.js
+++ src/components/Espanhol.js
@@ -3,20 +3,67 @@ const Espanhol = () => {
             <h1 className="all">Espanhol</h1>
                 <h3 className="subtitle">Você está na página Espanhol</h3>
 
+                <div class="row row-cols-1 row-cols-md-3 g-4" id="espaco-card">
+  <div class="col">
+    <div class="card">
+      <img src="https://img.freepik.com/premium-vector/spain-flag-design-waving-spanish-flag-made-satin-silk-fabric-vector-illustration_191567-876.jpg" 
+        class="card-img-top" alt="..."/>
+        <div class="card-body">
+            <h5 class="card-title">6ºANO</h5>
+                <p class="card-text">Para acessar as atividades da sua turma clique no botão abaixo.</p>
+                    <a class="btn btn-dark" target='_blank' href='https://drive.google.com/drive/folders/1zAC5Z7xtCD2_yBrRxdpslMtN97YliWr6?usp=share_link'>
+                        Atividades
+                    </a>
+        </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="https://img.freepik.com/premium-vector/spain-flag-design-waving-spanish-flag-made-satin-silk-fabric-vector-illustration_191567-876.jpg" 
+        class="card-img-top" alt="..."/>
+        <div class="card-body">
+            <h5 class="card-title">7ºANO</h5>
+                <p class="card-text">Para acessar as atividades da sua turma clique no botão abaixo.</p>
+                    <a class="btn btn-dark" target='_blank' href='https://drive.google.com/drive/folders/1hGA9pRl-mM2xJf04j5pL0uUxykCU_rXx?usp=share_link'>
+                        Atividades
+                    </a>
+        </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="https://img.freepik.com/premium-vector/spain-flag-design-waving-spanish-flag-made-satin-silk-fabric-vector-illustration_191567-876.jpg" 
+        class="card-img-top" alt="..."/>
+        <div class="card-body">
+            <h5 class="card-title">8ºANO</h5>
+                <p class="card-text">Para acessar as atividades da sua turma clique no botão abaixo.</p>
+                    <a class="btn btn-dark" target='_blank' href='https://drive.google.com/drive/folders/1vUkD3DU9T-x31C44Xv0MGL-pguiEnX6q?usp=share_link'>
+                        Atividades
+                    </a>
+        </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="https://img.freepik.com/premium-vector/spain-flag-design-waving-spanish-flag-made-satin-silk-fabric-vector-illustration_191567-876.jpg" 
+        class="card-img-top" alt="..."/>
+        <div class="card-body">
+            <h5 class="card-title">9ºANO</h5>
+                <p class="card-text">Para acessar as atividades da sua turma clique no botão abaixo.</p>
+                    <a class="btn btn-dark" target='_blank' href='https://drive.google.com/drive/folders/1dDEtTn2P5qUk7QdAZJ03xaD52D362IFu?usp=share_link'>
+                        Atividades
+                    </a>
+        </div>
+    </div>
+  </div>
 
-                
-                <p className="texto">
-                    Boa noite, este é um texto de teste. Boa noite, este é um texto de teste.
-                    Boa noite, este é um texto de teste. Boa noite, este é um texto de teste.
-                    Boa noite, este é um texto de teste. Boa noite, este é um texto de teste.
-                    Boa noite, este é um texto de teste. Boa noite, este é um texto de teste.
-                    Boa noite, este é um texto de teste. Boa noite, este é um texto de teste.
-                    Boa noite, este é um texto de teste. Boa noite, este é um texto de teste.
-                    Boa noite, este é um texto de teste. Boa noite, este é um texto de teste.
-                    Boa noite, este é um texto de teste. Boa noite, este é um texto de teste.
-                    Boa noite, este é um texto de teste. Boa noite, este é um texto de teste.
-                    Boa noite, este é um texto de teste. Boa noite, este é um texto de teste.
-                </p>
+
+
+</div>
+           
+
+
+
     
     </>
 };
